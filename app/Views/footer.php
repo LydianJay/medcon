d52c90bc08d7d3bee7d1e1262b535036a1356c27
LydianJay <lloydjayedradan@gmail.com>
2024-10-01 23:08:02 +0800

    separate footer and header

---
+++ app/Views/footer.php
@@ -0,0 +1,14 @@
+    <!--   Core JS Files   -->
+    <script src="<?php echo base_url() ?>/assets/js/core/popper.min.js"></script>
+    <script src="<?php echo base_url() ?>/assets/js/core/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>/assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="<?php echo base_url() ?>/assets/js/plugins/smooth-scrollbar.min.js"></script>
+
+   
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="<?php echo base_url() ?>/assets/js/material-dashboard.min.js?v=3.1.0"></script>
+</body>
+
+</html>
